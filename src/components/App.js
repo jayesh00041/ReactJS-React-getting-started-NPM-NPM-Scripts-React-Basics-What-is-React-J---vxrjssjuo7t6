b69eea2368b9cfe314b290a44cc6be2a057ec08d
jayesh00041 <jayesh00041@gmail.com>
2022-07-12 18:27:24 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,26 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import slides from "../data";
 
-const App = () => {
+const App = (props) => {
   let [slide_no, set_slide] = useState(0);
   function restart() {
     set_slide(0);
   }
   function prev() {
-    set_slide(slide_no--);
+    set_slide(--slide_no);
   }
   function next() {
-    set_slide(slide_no++);
+    console.log(slide_no);
+    // 
+    console.log(slide_no);
   }
   return (
     <div>
-      <h1 data-testid="title">{slides[slide_no].title}</h1>
-      <p data-testid="text">{slides[slide_no].text}</p>
-      <button data-testid="button-restart" onclick="{restart}" disabled = { slide_no === 0 }>Restart</button>
-      <button data-testid="button-prev" onclick="{prev}" disabled = { slide_no === 0 }>Prev</button>
-      <button data-testid="button-next" onclick="{next}" disabled = { slide_no === slides.length-1 }>Next</button>
+      <h1 data-testid="title">Hello{props.slides[slide_no].title}</h1>
+      <p data-testid="text">{props.slides[slide_no].text}</p>
+      <button data-testid="button-restart" onClick={()=>{set_slide(0);}} disabled = { slide_no === 0 }>Restart</button>
+      <button data-testid="button-prev" onClick={()=>{set_slide(--slide_no);}} disabled = { slide_no === 0 }>Prev</button>
+      <button data-testid="button-next" onClick={()=>{set_slide(++slide_no);}} disabled = { slide_no === props.slides.length-1 }>Next</button>
     </div>
   )
 }
