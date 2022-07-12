19c1b45f808e0e01db7ae00dbac88e18e001a6ea
jayesh00041 <jayesh00041@gmail.com>
2022-07-12 17:18:46 +0530

    submit assignment4

--- src/components/App.js
+++ src/components/App.js
@@ -18,8 +18,8 @@ const App = () => {
       <h1 data-testid="title">{slides[slide_no].title}</h1>
       <p data-testid="text">{slides[slide_no].text}</p>
       <button data-testid="button-restart" onclick="{restart}" disabled = { slide_no === 0 }>Restart</button>
-      <button data-testid="prevButton" onclick="{prev}" disabled = { slide_no === 0 }>Prev</button>
-      <button data-testid="nextButton" onclick="{next}" disabled = { slide_no === slides.length-1 }>Next</button>
+      <button data-testid="button-prev" onclick="{prev}" disabled = { slide_no === 0 }>Prev</button>
+      <button data-testid="button-next" onclick="{next}" disabled = { slide_no === slides.length-1 }>Next</button>
     </div>
   )
 }
